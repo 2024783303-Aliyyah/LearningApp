73ed49c57f46f4fae2be311cd9ba3140a2f28be7
ALIYYAH <2024783303@student.uitm.edu.my>
2025-12-23 13:16:15 +0800

    add recycler view on sciencelessons layout and set list on sciencelessons java

--- MicroLearningQuizApp/app/src/main/java/com/example/microlearningquizapp/ScienceLessons.java
+++ MicroLearningQuizApp/app/src/main/java/com/example/microlearningquizapp/ScienceLessons.java
@@ -7,18 +7,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ScienceLessons extends AppCompatActivity {
 
+    RecyclerView rvLessons;
+    ArrayList<Lesson> lessonList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_science_lessons);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.science), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        rvLessons = findViewById(R.id.rvLessons);
+        rvLessons.setLayoutManager(new LinearLayoutManager(this));
+
+        lessonList = new ArrayList<>();
+
+        lessonList.add(new Lesson("Living vs Non-Living"));
+        lessonList.add(new Lesson("Five senses"));
+
+        LessonAdapter adapter = new LessonAdapter(this, lessonList);
+        rvLessons.setAdapter(adapter);
     }
 }
