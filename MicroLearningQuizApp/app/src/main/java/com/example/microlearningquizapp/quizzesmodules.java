c63a1f655fec8949d072161ef8258e3f4581777a
ALIYYAH <2024783303@student.uitm.edu.my>
2026-01-01 16:35:24 +0800

    edit quiz modules

--- MicroLearningQuizApp/app/src/main/java/com/example/microlearningquizapp/quizzesmodules.java
+++ MicroLearningQuizApp/app/src/main/java/com/example/microlearningquizapp/quizzesmodules.java
@@ -15,7 +15,7 @@ public class quizzesmodules extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_quizzesmodules);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.quizzesmodules), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
