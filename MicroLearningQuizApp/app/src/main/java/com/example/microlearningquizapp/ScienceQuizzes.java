df4d9629ab96e517a5d4671bf4e43599897f8eb9
Miza Syahirah <2024991721@student.uitm.edu.my>
2026-01-05 19:46:19 +0800

    add quiz navigation in ScienceQuizzes.java

--- MicroLearningQuizApp/app/src/main/java/com/example/microlearningquizapp/ScienceQuizzes.java
+++ MicroLearningQuizApp/app/src/main/java/com/example/microlearningquizapp/ScienceQuizzes.java
@@ -24,6 +24,17 @@ public class ScienceQuizzes extends AppCompatActivity {
         });
     }
 
+    public void opensciencequiz(View view) {
+        int id = view.getId();
+        if (id == R.id.AnimalQuiz) {
+            startActivity(new Intent(this, AnimalQuiz.class));
+        } else if (id == R.id.BodyQuiz) {
+            startActivity(new Intent(this, BodyQuiz.class));
+        } else if (id == R.id.SpaceQuiz) {
+            startActivity(new Intent(this, SpaceQuiz.class));
+        }
+    }
+
     public void openMenu(View view)
     {
         int id = view.getId();
@@ -42,17 +53,4 @@ public class ScienceQuizzes extends AppCompatActivity {
         finish();
     }
 
-    /*public void quiz1(View view) {
-        Intent intent = new Intent(this, Quiz1.class);
-        startActivity(intent);
-
-    }
-    public void quiz2(View view) {
-        Intent intent = new Intent(this, Quiz2.class);
-        startActivity(intent);
-    }
-    public void quiz3(View view) {
-        Intent intent = new Intent(this, Quiz3.class);
-        startActivity(intent);
-    }*/
 }
