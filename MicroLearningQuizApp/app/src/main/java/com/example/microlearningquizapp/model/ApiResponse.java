95a092a0802b7e1def4d9a5ed2fc2d6358a19e56
farha <2024757467@student.uitm.edu.my>
2026-01-18 00:57:03 +0800

    add ApiResponse.java

---
+++ MicroLearningQuizApp/app/src/main/java/com/example/microlearningquizapp/model/ApiResponse.java
@@ -0,0 +1,9 @@
+package com.example.microlearningquizapp.model;
+
+public class ApiResponse {
+    private String status;
+    private String message;
+
+    public String getStatus() { return status; }
+    public String getMessage() { return message; }
+}
