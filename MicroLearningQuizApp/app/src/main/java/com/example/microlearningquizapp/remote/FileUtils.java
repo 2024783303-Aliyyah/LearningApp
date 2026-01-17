3521597c18575c623bf98fa9a4b2abe379e8013e
farha <2024757467@student.uitm.edu.my>
2026-01-18 00:57:36 +0800

    add FileUtils.java

---
+++ MicroLearningQuizApp/app/src/main/java/com/example/microlearningquizapp/remote/FileUtils.java
@@ -0,0 +1,75 @@
+package com.example.microlearningquizapp;
+
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+
+import java.io.File;
+
+public class FileUtils {
+
+    public static String getPath(Context context, Uri uri) {
+        if (uri == null) return null;
+
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                String[] split = docId.split(":");
+                if ("primary".equalsIgnoreCase(split[0])) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            } else if (isDownloadsDocument(uri)) {
+                String id = DocumentsContract.getDocumentId(uri);
+                Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.parseLong(id));
+                return getDataColumn(context, contentUri, null, null);
+            } else if (isMediaDocument(uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                String[] split = docId.split(":");
+                Uri contentUri = null;
+                switch (split[0]) {
+                    case "image": contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI; break;
+                    case "video": contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI; break;
+                    case "audio": contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI; break;
+                }
+                return getDataColumn(context, contentUri, "_id=?", new String[]{split[1]});
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        String column = "_data";
+        String[] projection = { column };
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null) cursor.close();
+        }
+        return null;
+    }
+
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+}
